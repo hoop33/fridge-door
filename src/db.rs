316f0efe98818645443ccdfd6e5cded43ae384f6
Rob Warner <rwarner@grailbox.com>
2023-01-17 15:39:41 -0500

    feat: add offset, limit, and expiration

--- src/db.rs
+++ src/db.rs
@@ -1,15 +1,18 @@
-use sqlx::types::chrono::NaiveDateTime;
-use rocket::{Build, Rocket};
 use rocket::fairing::{self, AdHoc};
-use rocket::serde::{Deserialize, Serialize};
 use rocket::serde::json::Json;
-use rocket_db_pools::{Connection, Database, sqlx};
+use rocket::serde::{Deserialize, Serialize};
+use rocket::{Build, Rocket};
+use rocket_db_pools::{sqlx, Connection, Database};
+use sqlx::types::chrono::NaiveDateTime;
 
 #[derive(Database)]
 #[database("fridge-door")]
 struct Db(sqlx::SqlitePool);
 
 type Result<T, E = rocket::response::Debug<sqlx::Error>> = std::result::Result<T, E>;
+
+const DEFAULT_QUERY_LIMIT: u32 = 20;
+const DEFAULT_QUERY_OFFSET: u32 = 0;
 
 #[derive(Clone, Debug, Deserialize, Serialize)]
 #[serde(crate = "rocket::serde")]
@@ -20,13 +23,29 @@ struct Message {
     font_color: Option<String>,
     font_family: Option<String>,
     created_at: NaiveDateTime,
-    expires_at: NaiveDateTime
+    expires_at: NaiveDateTime,
 }
 
-#[get("/")]
-async fn list(mut db: Connection<Db>) -> Result<Json<Vec<Message>>> {
-    let messages = sqlx::query_as!(Message, "select * from messages").fetch_all(&mut *db)
-        .await?;
+#[get("/?<limit>&<offset>&<include_expired>")]
+async fn list(
+    mut db: Connection<Db>,
+    limit: Option<u32>,
+    offset: Option<u32>,
+    include_expired: Option<u32>,
+) -> Result<Json<Vec<Message>>> {
+    let limit = limit.unwrap_or(DEFAULT_QUERY_LIMIT);
+    let offset = offset.unwrap_or(DEFAULT_QUERY_OFFSET);
+    let include_expired = include_expired.unwrap_or(0);
+
+    let messages = sqlx::query_as!(
+        Message,
+        "select * from messages where id > ? and (? = 1 or date('now') < expires_at) limit ?",
+        offset,
+        include_expired,
+        limit
+    )
+    .fetch_all(&mut *db)
+    .await?;
 
     Ok(Json(messages))
 }
@@ -46,7 +65,8 @@ async fn run_migrations(rocket: Rocket<Build>) -> fairing::Result {
 
 pub fn stage() -> AdHoc {
     AdHoc::on_ignite("SQLx Stage", |rocket| async {
-        rocket.attach(Db::init())
+        rocket
+            .attach(Db::init())
             .attach(AdHoc::try_on_ignite("SQLx Migrations", run_migrations))
             .mount("/messages", routes![list])
     })
