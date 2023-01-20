256bb7cf9cf05312e36fccd1beb616217ef96b6f
Rob Warner <rwarner@grailbox.com>
2023-01-19 19:58:05 -0500

    feat: add cors

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,30 @@ mod db;
 #[macro_use]
 extern crate rocket;
 
+use rocket_cors::{AllowedOrigins, CorsOptions};
+use std::str::FromStr;
+
+#[get("/")]
+fn cors<'a>() -> &'a str {
+    "Hello CORS!"
+}
+
 #[launch]
 fn rocket() -> _ {
-    rocket::build().attach(db::stage())
+    let cors = CorsOptions::default()
+        .allowed_origins(AllowedOrigins::all())
+        .allowed_methods(
+            vec!["Get", "Post", "Options", "Delete"]
+                .into_iter()
+                .map(|s| FromStr::from_str(s).unwrap())
+                .collect(),
+        )
+        .allow_credentials(true)
+        .to_cors()
+        .unwrap();
+
+    rocket::build()
+        .mount("/", routes![cors])
+        .attach(cors)
+        .attach(db::stage())
 }
